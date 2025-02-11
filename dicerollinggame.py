69db84be94dd3fc418e6f129bfe16ea63e8afcc0
gagsand <sandhu-15@hotmail.com>
2025-02-10 20:23:58 -0800

    Update dicerollinggame.py

--- dicerollinggame.py
+++ dicerollinggame.py
@@ -2,18 +2,15 @@
 
 import random
 
-# answers = {"n": no, "y": yes}
-
 player_input = input("Roll the dice? (y/n): ").lower()
 
-if player_input != "y" or "no":
-    print("invalid entry, try again")
 
 if player_input == "y":
     while True:
-        roll_dice = random.randint(1, 6)
-        print(roll_dice)
-        print(input("play again?: (y/n): "))
+        roll_dice1 = random.randint(1, 6)
+        roll_dice2 = random.randint(1, 6)
+        print((roll_dice1, roll_dice2))
+        break
 
 if player_input == "n":
     print("thanks for playing")
