b6e96283847b74bae2f87686fd7f492171c25bbc
gagsand <sandhu-15@hotmail.com>
2025-02-15 10:37:36 -0800

    Update dicerollinggame.py

--- dicerollinggame.py
+++ dicerollinggame.py
@@ -1,16 +1,30 @@
 # programming with Mosh Youtube tutorial called "python projects for beginnners - master problem solving!"
 
+# Loop
+# ask user a question: roll the dice?
+# if user enters y
+#   generate two random numbers
+#   print them
+# If user enters n
+#   print thank you message
+#   Terminate
+# Else
+#   print invalid choice
+
+
 import random
 
-player_input = input("Roll the dice? (y/n): ").lower()
+while True:
+    player_input = input("Would you like to roll the dice? (y/n): ").lower()
 
-
-if player_input == "y":
-    while True:
+    if player_input == "y":
         roll_dice1 = random.randint(1, 6)
         roll_dice2 = random.randint(1, 6)
-        print((roll_dice1, roll_dice2))
+        print(f'({roll_dice1}, {roll_dice2})')
+
+    elif player_input == "n":
+        print("thanks for playing")
         break
 
-if player_input == "n":
-    print("thanks for playing")
+    else:
+        print("Invalid choice, please enter y or n")
